e55a839658c1619f9af68ed1d563a44983cfe4f5
Oleg Yadrov <wearyinside@gmail.com>
2015-09-15 09:04:49 -0700

    Changed version number
    v1.2.4

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     qInstallMessageHandler(&MessageHandler::handler);
     QGuiApplication app(argc, argv);
     app.setApplicationName("QML Creator");
-    app.setApplicationVersion("1.2.3");
+    app.setApplicationVersion("1.2.4");
     app.setOrganizationName("wearyinside");
     app.setOrganizationDomain("com.wearyinside.qmlcreator");
 
